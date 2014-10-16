d13fac1b5027546e2c2be441661b00a600b448cc
Neil McGettigan <c12810959@mydit.ie>
2014-10-16 13:16:01 +0000

    added simple new functions to app.py

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -2,9 +2,23 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello World'
+  
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
