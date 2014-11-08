3c9563e38519c9121f0ae825bef9fe72b759e3fd
Neil McGettigan <c12810959@mydit.ie>
2014-11-08 11:19:03 +0200

    forgot to push to github. modified files from lab are here.

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask import request
 import os, sys
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -63,6 +69,22 @@ def run_eu2():
   	
 	result=" "+(str)(tally)+"\n"
   	return result
+
+@app.route('/listqueues')
+def listQueues():
+	conn1 = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+	conn2 = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+
+        result=""
+        rs = conn1.get_all_queues()
+        for q in rs:
+                result=result + (str)(q.id) + "\n"
+
+        rs = conn2.get_all_queues()
+        for q in rs:
+                result=result + (str)(q.id) + "\n"
+
+        return result
 
 
 if __name__ == "__main__":
